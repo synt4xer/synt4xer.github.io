deb4fd0175262ef1c937a6a5ee1842b2a291e9ba
Ferrel John Fernando <f.syntaxer@gmail.com>
2024-11-20 14:48:49 +0700

    feat: Add components, layout, and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
-import { defineConfig } from 'astro/config';
+// import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+// // https://astro.build/config
+// export default defineConfig({});
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://your-domain.com',
+  integrations: [
+    tailwind(),
+    sitemap(),
+  ],
+});
