9828ce2b0505ff96e7c5781a12b7f654d13a2857
Ferrel John Fernando <f.syntaxer@gmail.com>
2024-11-21 14:23:55 +0700

    ref: Update header and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
-// import { defineConfig } from 'astro/config';
-
-// // https://astro.build/config
-// export default defineConfig({});
+// https://astro.build/config
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
